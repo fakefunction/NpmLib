d6d214c6c7065ba8112e210994d2765ca8967dae
Sam <contactsamie@gmail.com>
2019-05-21 12:29:36 -0700

    update

--- index.js
+++
@@ -1,3 +0,0 @@
-exports.calculatorLib = function() {
-  console.log("This is a message from the calculatorLib package");
-};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var app = function(){
+
+}
+app.someData = { };
+module.exports = app;
